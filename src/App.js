f7ee2e314521967b914cfb4c5707e7d6d9d48225
hthi4326 <hthi4326@gmail.com>
2022-10-07 21:51:52 +1100

    Commit 1

--- src/App.js
+++ src/App.js
@@ -7,16 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Enter <code>the matrix</code>.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Hannah is on the internet!
+        </p>
       </header>
     </div>
   );
